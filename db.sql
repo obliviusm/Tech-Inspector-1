675e3281544d3cbe4bf54fa6163ff4d086346c35
Dima <0979029562@mail.ru>
2012-04-10 21:21:13 +0300

    Add data base dump

---
+++ db.sql
@@ -0,0 +1,197 @@
+-- MySQL dump 10.13  Distrib 5.5.22, for Win32 (x86)
+--
+-- Host: localhost    Database: tech_inspector
+-- ------------------------------------------------------
+-- Server version	5.5.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `attributes`
+--
+
+DROP TABLE IF EXISTS `attributes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attributes` (
+  `attribute_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_id` int(11) NOT NULL,
+  `attribute_name` varchar(45) NOT NULL,
+  `required` tinyint(1) NOT NULL,
+  PRIMARY KEY (`attribute_id`),
+  UNIQUE KEY `attribute_id_UNIQUE` (`attribute_id`),
+  KEY `fk_Attributes_Types1` (`type_id`),
+  CONSTRAINT `fk_Attributes_Types1` FOREIGN KEY (`type_id`) REFERENCES `types` (`type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attributes`
+--
+
+LOCK TABLES `attributes` WRITE;
+/*!40000 ALTER TABLE `attributes` DISABLE KEYS */;
+INSERT INTO `attributes` VALUES (1,1,'Монітор',1),(2,1,'Клавіатура',1),(3,1,'Миш',1),(4,1,'Процесор',1),(5,1,'Відеокарта',1),(6,1,'HDD',1),(7,1,'RAM',1),(8,1,'Привід',1),(9,1,'USB',0),(10,1,'Bluetooth',0),(11,1,'Wi-fi',0),(12,2,'Виробник',1),(13,2,'Модель',1),(14,2,'Срок експлуатації',0),(15,3,'Виробник',1),(16,3,'Модель',1),(17,3,'Тип (1 - струйний, 2 - лазерний)',0),(18,9,'Тип обладнання',1);
+/*!40000 ALTER TABLE `attributes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipments`
+--
+
+DROP TABLE IF EXISTS `equipments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `equipments` (
+  `equipment_id` int(11) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL,
+  `state_id` int(11) NOT NULL,
+  `price` float DEFAULT NULL,
+  `purchase_date` datetime DEFAULT NULL,
+  `placement_date` datetime DEFAULT NULL,
+  `warranty_end_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`equipment_id`),
+  UNIQUE KEY `equipments_id_UNIQUE` (`equipment_id`),
+  KEY `fk_Equipments_Types` (`type_id`),
+  KEY `fk_Equipments_Locations1` (`location_id`),
+  KEY `fk_Equipments_States1` (`state_id`),
+  CONSTRAINT `fk_Equipments_Types` FOREIGN KEY (`type_id`) REFERENCES `types` (`type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Equipments_Locations1` FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Equipments_States1` FOREIGN KEY (`state_id`) REFERENCES `states` (`state_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipments`
+--
+
+LOCK TABLES `equipments` WRITE;
+/*!40000 ALTER TABLE `equipments` DISABLE KEYS */;
+INSERT INTO `equipments` VALUES (1,1,1,1,1000,'2010-03-20 07:00:00',NULL,NULL),(2,9,10,1,0,'2012-04-09 00:00:00','2012-04-09 00:00:00','2012-04-09 00:00:00'),(3,9,10,1,0,NULL,NULL,NULL),(45,9,10,1,0,NULL,NULL,NULL),(110535,3,8,2,100,'2012-04-10 11:21:24',NULL,NULL),(124667,2,10,1,0,NULL,NULL,NULL),(10400001,2,9,1,10000,'2012-04-10 10:03:04','2012-04-10 10:03:04','2012-04-10 10:03:04'),(13053453,9,7,1,100.56,'2012-04-09 00:00:00','2012-04-09 00:00:00','2012-04-09 00:00:00'),(14534535,2,10,1,0,NULL,NULL,NULL),(111111111,9,10,1,0,'2012-04-10 10:17:21','0001-01-01 00:00:00','0001-01-01 00:00:00'),(123456789,9,10,3,56.45,NULL,'2012-04-10 10:36:25',NULL);
+/*!40000 ALTER TABLE `equipments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipments_has_attributes`
+--
+
+DROP TABLE IF EXISTS `equipments_has_attributes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `equipments_has_attributes` (
+  `equipment_id` int(11) NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `attribute_value` varchar(45) DEFAULT NULL,
+  KEY `fk_Equipments_has_Attributes_Attributes1` (`attribute_id`),
+  KEY `fk_Equipments_has_Attributes_Equipments1` (`equipment_id`),
+  CONSTRAINT `fk_Equipments_has_Attributes_Equipments1` FOREIGN KEY (`equipment_id`) REFERENCES `equipments` (`equipment_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Equipments_has_Attributes_Attributes1` FOREIGN KEY (`attribute_id`) REFERENCES `attributes` (`attribute_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipments_has_attributes`
+--
+
+LOCK TABLES `equipments_has_attributes` WRITE;
+/*!40000 ALTER TABLE `equipments_has_attributes` DISABLE KEYS */;
+INSERT INTO `equipments_has_attributes` VALUES (1,1,'1'),(1,18,'Тип'),(13053453,18,'test'),(2,18,'фыафыа'),(3,18,'вапвап'),(45,18,'апркввекр44'),(124667,12,'Проектор'),(124667,13,'Модель'),(124667,14,''),(10400001,12,'Toshiba'),(10400001,13,'IC0111'),(10400001,14,''),(111111111,18,'тест4'),(123456789,18,'ываыва'),(14534535,14,''),(14534535,13,'ыв'),(14534535,12,'ыфв'),(110535,15,'Панасони'),(110535,16,'СТОДЕ120ЕМ'),(110535,17,'1');
+/*!40000 ALTER TABLE `equipments_has_attributes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `locations` (
+  `location_id` int(11) NOT NULL AUTO_INCREMENT,
+  `location_name` varchar(32) NOT NULL,
+  PRIMARY KEY (`location_id`),
+  UNIQUE KEY `idLocations_UNIQUE` (`location_id`),
+  UNIQUE KEY `location_name_UNIQUE` (`location_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES (10,'229/1'),(11,'229/2'),(12,'229/3'),(13,'323'),(7,'416/1'),(8,'416/2'),(9,'417'),(14,'422'),(6,'426'),(5,'428'),(3,'429/1'),(4,'429/2'),(1,'430'),(2,'433');
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `states`
+--
+
+DROP TABLE IF EXISTS `states`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `states` (
+  `state_id` int(11) NOT NULL AUTO_INCREMENT,
+  `state_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`state_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `states`
+--
+
+LOCK TABLES `states` WRITE;
+/*!40000 ALTER TABLE `states` DISABLE KEYS */;
+INSERT INTO `states` VALUES (1,'Справний'),(2,'Частково несправний'),(3,'Повністю несправний');
+/*!40000 ALTER TABLE `states` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `types`
+--
+
+DROP TABLE IF EXISTS `types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `types` (
+  `type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(32) NOT NULL,
+  PRIMARY KEY (`type_id`),
+  UNIQUE KEY `type_id_UNIQUE` (`type_id`),
+  UNIQUE KEY `type_name_UNIQUE` (`type_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `types`
+--
+
+LOCK TABLES `types` WRITE;
+/*!40000 ALTER TABLE `types` DISABLE KEYS */;
+INSERT INTO `types` VALUES (7,'KVM'),(8,'UPS'),(6,'WiFi'),(9,'Інше'),(1,'Комп\'ютер'),(5,'Комутатор'),(3,'Принтер'),(2,'Проектор'),(4,'Сервер');
+/*!40000 ALTER TABLE `types` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-04-10 21:19:52
